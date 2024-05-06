c0a297d151adffaf32bfdf5fc3e2d0afe2c67a99
Shobhit Singh <majin.shobhitrawat53@gmail.com>
2024-05-06 22:35:35 +0530

    first code push
    first code only widgit work is completed

---
+++ app/src/main/java/com/example/widgetsapplication/StackRemoteViewsFactory.kt
@@ -0,0 +1,54 @@
+package com.example.widgetsapplication
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.widget.RemoteViews
+import android.widget.RemoteViewsService
+import androidx.core.os.bundleOf
+
+internal class StackRemoteViewsFactory(private val context: Context) :
+    RemoteViewsService.RemoteViewsFactory {
+
+    private val widgetItems = ArrayList<Bitmap>()
+
+    override fun onCreate() {
+
+    }
+
+    override fun onDataSetChanged() {
+        widgetItems.apply {
+            add(BitmapFactory.decodeResource(context.resources, R.drawable.darth_vader))
+            add(BitmapFactory.decodeResource(context.resources, R.drawable.star_wars_logo))
+            add(BitmapFactory.decodeResource(context.resources, R.drawable.storm_trooper))
+            add(BitmapFactory.decodeResource(context.resources, R.drawable.starwars))
+            add(BitmapFactory.decodeResource(context.resources, R.drawable.falcon))
+        }
+    }
+
+    override fun onDestroy() {
+    }
+
+    override fun getCount(): Int = widgetItems.size
+
+    override fun getViewAt(position: Int): RemoteViews {
+        val rv = RemoteViews(context.packageName, R.layout.widget_item)
+        rv.setImageViewBitmap(R.id.image_view, widgetItems[position])
+
+        val extras = bundleOf(ImagesBannerWidget.EXTRA_ITEM to position)
+        val fillIntent = Intent()
+        fillIntent.putExtras(extras)
+
+        rv.setOnClickFillInIntent(R.id.image_view, fillIntent)
+        return rv
+    }
+
+    override fun getLoadingView(): RemoteViews? = null
+
+    override fun getViewTypeCount(): Int = 1
+
+    override fun getItemId(position: Int): Long = 0
+
+    override fun hasStableIds(): Boolean = false
+}
