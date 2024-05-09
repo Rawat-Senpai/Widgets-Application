30e5b6c453223fc9be11492aa3c57df5a51ca8f0
Shobhit Singh <majin.shobhitrawat53@gmail.com>
2024-05-09 18:47:52 +0530

    just cange a state

--- app/src/main/java/com/example/widgetsapplication/repository/AnimeRepository.kt
+++ app/src/main/java/com/example/widgetsapplication/repository/AnimeRepository.kt
@@ -1,12 +1,15 @@
 package com.example.widgetsapplication.repository
 
+import androidx.lifecycle.ViewModel
 import com.example.widgetsapplication.api.AnimeApi
 import com.example.widgetsapplication.utils.NetworkResult
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
-class AnimeRepository @Inject constructor(private val animeApi:AnimeApi) {
+class AnimeRepository @Inject constructor(private val animeApi:AnimeApi) : ViewModel() {
 
     private val _akatsukiResponse = MutableStateFlow<NetworkResult<>>()
+    val akatsukiResponse : StateFlow<NetworkResult<>>
 
 }
